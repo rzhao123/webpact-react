25fcb9057d82b5265e3d4da5d3509e1ec4740d42
Ubuntu <ubuntu@nginx>
2018-07-05 10:06:00 +0800

    modify index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,22 @@ let environment = process.env.NODE_ENV;
 
 console.log(environment);
 
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Robin',
+    lastName: 'Zhao'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
+
 ReactDOM.render(
-  <h1>Hello, React!</h1>,
+  element,
   document.getElementById('root')
 );
