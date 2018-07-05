9d195ef77972c3a44b1e4f6e71eb563c9a664cdb
Ubuntu <ubuntu@nginx>
2018-07-05 10:59:35 +0800

    add component

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Welcome from './components/welcome.component';
 
 let environment = process.env.NODE_ENV;
 
 console.log(environment);
 
-function formatName(user) {
-    if (user) {
-        return user.firstName + ' ' + user.lastName;
-    }
-    return 'React';
-}
-
-const user = {
-    firstName: 'Robin',
-    lastName: 'Zhao'
-};
-
-const element = (
-    <h1>
-        Hello, {formatName(user)}!
-    </h1>
-);
+const element = <Welcome name="React" />;
 
 ReactDOM.render(
   element,
